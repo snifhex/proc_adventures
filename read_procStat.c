d33da2fa9fd74bb9fc3f58ebafa3c7841285e5d3
Sachin Tripathi <sachintripathis84@gmail.com>
2019-06-09 22:37:23 +0530

    Update read_procStat.c

--- read_procStat.c
+++ read_procStat.c
@@ -11,25 +11,15 @@ int main(int argc, char **argv)
 	char file_name[100];
 	char text[150];
 	FILE *fp;
-	char *p;
-
 	pid = argv[1];
 	sprintf(file_name, "/proc/%s/stat", pid);
 	// printf(file_name);
 	fp = fopen(file_name, "r");
 	while ((ch = fgetc(fp)) != EOF)
 	{
-		// printf("%c\n", ch);
 		text[i] = ch;
 		i++;
 	}
-
-	// p = strtok(text, " ");
-	// if (p)
-	// {
-		
-	// }
-	// puts(text);
 	fclose(fp);
 	return 0;
 }
